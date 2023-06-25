4c05248e75281efb6b0a5f0da34184610e9a1b13
Samir <118690585+sr5434@users.noreply.github.com>
2023-06-25 20:11:23 +0000

    Add auth(currently broken)

--- copywriter/src/app/app-routing.module.ts
+++ copywriter/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { AppComponent } from './app.component';
+
+// route guard
+import { AuthGuard } from './guards/auth.guard';
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register-user', component: SignUpComponent },
+  { path: 'dashboard', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- copywriter/src/app/app.module.ts
+++ copywriter/src/app/app.module.ts
@@ -4,18 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { WriterService } from './writer.service'
+
+import { WriterService } from './writer.service';
+import { AuthService } from "./auth.service";
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
   ],
-  providers: [WriterService],
+  providers: [WriterService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
